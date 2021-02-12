74e25af4851986c1edb4d3dd730a9f9f18e87b1c
nazareth <naiger67@gmail.com>
2021-02-12 19:22:08 -0400

    cambios

--- src/app/layouts/components/auth-button-action/auth-button-action.event.ts
+++ src/app/layouts/components/auth-button-action/auth-button-action.event.ts
@@ -1,10 +1,19 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { ReplaySubject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class AuthButtonActionEvent {
   public event$: ReplaySubject<any> = new ReplaySubject<any>();
-  constructor() {
+  constructor(private router: Router) {
 
+  }
+
+  login() {
+    this.router.navigate(['auth/login']);
+  }
+
+  register() {
+    this.router.navigate(['auth/register']);
   }
 }
