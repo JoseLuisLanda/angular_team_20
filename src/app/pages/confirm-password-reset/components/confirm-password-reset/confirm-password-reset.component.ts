5116ac3344606e39fc81261d08d53347a76d96fb
nazareth <naiger67@gmail.com>
2021-02-12 21:24:57 -0400

    cambios

--- src/app/pages/confirm-password-reset/components/confirm-password-reset/confirm-password-reset.component.ts
+++ src/app/pages/confirm-password-reset/components/confirm-password-reset/confirm-password-reset.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ConfirmPasswordReset } from 'src/app/firebase/auth/confirm-password-reset';
 import { AuthButtonActionEvent } from 'src/app/layouts/components/auth-button-action/auth-button-action.event';
 
 @Component({
@@ -10,12 +11,17 @@ import { AuthButtonActionEvent } from 'src/app/layouts/components/auth-button-ac
 export class ConfirmPasswordResetComponent implements OnInit {
 
   constructor(
+    private _confirmPasswordReset: ConfirmPasswordReset,
     private authButtonActionEvent: AuthButtonActionEvent,
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    let params = this.activatedRoute.snapshot.params;
+    let params = this.getParams();
     this.resetAuthButtonAction();
+  }
+
+  protected getParams() {
+    return this.activatedRoute.snapshot.params;
   }
 
   protected resetAuthButtonAction() {
@@ -27,6 +33,13 @@ export class ConfirmPasswordResetComponent implements OnInit {
 
   confirmarPasswordResetAction(form: any) {
     console.log("ConfirmPasswordResetComponent", form);
+    let params = this.getParams();
+    let code = params['code'];
+    this.confirmPasswordReset(code, form.password);
+  }
+
+  confirmPasswordReset(code: any, password: any) {
+    this._confirmPasswordReset.handle(code, password);
   }
 
 }
