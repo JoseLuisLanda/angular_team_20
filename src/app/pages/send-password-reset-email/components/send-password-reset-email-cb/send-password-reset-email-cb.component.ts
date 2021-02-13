ab12b3b2ead7bae00bc4cbafa1bdba8f2988f5b8
nazareth <naiger67@gmail.com>
2021-02-12 20:35:29 -0400

    cambios

--- src/app/pages/send-password-reset-email/components/send-password-reset-email-cb/send-password-reset-email-cb.component.ts
+++ src/app/pages/send-password-reset-email/components/send-password-reset-email-cb/send-password-reset-email-cb.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-send-password-reset-email-cb',
@@ -9,9 +10,13 @@ export class SendPasswordResetEmailCbComponent implements OnInit {
   @Input() debug: boolean = false;
   public code = "";
   
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  confirmar() {
+    this.router.navigate(['auth/confirm-password-reset']);
   }
 
 }
