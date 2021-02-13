4dddf3326fa9a0f101b82436efc8f7c6e9b4a083
nazareth <naiger67@gmail.com>
2021-02-12 20:10:04 -0400

    cambios

---
+++ src/app/pages/send-password-reset-email/components/send-password-reset-email/send-password-reset-email.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SendPasswordResetEmail } from 'src/app/firebase/auth/send-password-reset-email';
+import { AuthButtonActionEvent } from 'src/app/layouts/components/auth-button-action/auth-button-action.event';
+
+@Component({
+  selector: 'app-send-password-reset-email',
+  templateUrl: './send-password-reset-email.component.html',
+  styleUrls: ['./send-password-reset-email.component.css']
+})
+export class SendPasswordResetEmailComponent implements OnInit {
+
+  public debug = true;
+  public email = "";
+  public code = "";
+
+  constructor(
+    private authButtonActionEvent: AuthButtonActionEvent,
+    private activatedRoute: ActivatedRoute,
+    private _sendPasswordResetEmail: SendPasswordResetEmail) { }
+
+  ngOnInit(): void {
+    let params = this.activatedRoute.snapshot.params;
+    this.updateCorreo(params.email);
+    this.resetAuthButtonAction();
+    //this.sendPasswordResetEmail(params.email);
+  }
+
+  public updateCorreo(email: any) {
+    this.email = email;
+  }
+
+  protected resetAuthButtonAction() {
+    this.authButtonActionEvent.event$.next({
+      login: true,
+      register: false
+    });
+  }
+
+  protected sendPasswordResetEmail(email: any) {
+    this._sendPasswordResetEmail.handle(email);
+  }
+
+  
+
+}
