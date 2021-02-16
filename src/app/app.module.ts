5dfa6e53d621d5763eca8a2fd0a0c28c020795e2
nazareth <naiger67@gmail.com>
2021-02-16 11:02:42 -0400

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,14 @@ import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
-import { AuthNavbarComponent } from './layouts/components/auth-navbar/auth-navbar.component';
-import { AuthButtonActionComponent } from './layouts/components/auth-button-action/auth-button-action.component';
-import { DashboardNavbarComponent } from './layouts/components/dashboard-navbar/dashboard-navbar.component';
+import { DashboardNavbarComponent } from './layouts/dashboard-layout/components/dashboard-navbar/dashboard-navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthLayoutComponent,
     DashboardLayoutComponent,
-    AuthNavbarComponent,
-    AuthButtonActionComponent,
     DashboardNavbarComponent
   ],
   imports: [
